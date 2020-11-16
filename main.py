58ea8289e3eb3688a6cd94395c36a62f5bac9558
Zinaida <zinapanyak@mail.ru>
2020-11-16 14:45:15 +0500

    Добавляем комментарии

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def isfloat(value):
+def isfloat(value):  # Функция, определяющая, какое значение важности введено
     try:
         float(value)
         return True
@@ -9,24 +9,24 @@ def isfloat(value):
         return False
 
 
-def matrix(k):
-    M = np.ones([k, k])
+def matrix(k): 
+    M = np.ones([k, k])  # Создаем матрицу из единиц
     for i in range(0, k):
         for j in range(0, k):
-            if i < j:
+            if i < j:  # Все, что выше главной диагонали, меняем на введенные польз-лем значения
                 print("Насколько критерий", i + 1, "важнее чем", j + 1, "? Введите число от 0.1 до 9: ", end='')
                 a = input()
                 if isfloat(a) and (0.11 <= float(a) <= 9):
                     M[i, j] = float(a)
                     M[j, i] = 1 / float(a)
-                else:
+                else:  # Учитываем ошибки
                     print("Введены неверные данные. Попробуйте еще раз.")
                     print("Насколько критерий", i + 1, "важнее чем", j + 1, "? Введите число от 0.1 до 9: ", end='')
                     a = input()
                     if isfloat(a) and (0.11 <= float(a) <= 9):
                         M[i, j] = float(a)
                         M[j, i] = 1 / float(a)
-                    else:
+                    else:  # Если во второй раз введены неверные значения, устанавливаем важность = 1
                         print("Введены неверные данные. Критерии одинаково важны.")
                         a = 1
                         M[i, j] = float(a)
@@ -34,8 +34,8 @@ def matrix(k):
     return M
 
 
-def stroka(matrix):
-    stroka = matrix[:, :]
+def stroka(matrix):  # Функция, которая возвращает строку матрицы с посчитанными весовыми коэф-ми
+    stroka = matrix[:, :] 
     weight = stroka / stroka.sum()
     return weight
 
@@ -47,7 +47,7 @@ def main():
         weight = stroka(M)
         for i in range(int(k)):
             print("Вес критерия №", i + 1, "=", np.round(weight[i].sum(), 2))
-    else:
+    else:  # Обработка шибок
         print("Введены неверные данные. Попробуйте еще раз. ")
         main()
 
